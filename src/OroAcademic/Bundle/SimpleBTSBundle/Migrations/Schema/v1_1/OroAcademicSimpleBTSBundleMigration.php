8f4387ea84fd13c1399dd111a8420f02dd1b30a3
Semyen Pavlov <pavlov.semyen@gmail.com>
2016-04-14 10:00:07 +0300

    OOT-1212: Email activity for Issue

--- src/OroAcademic/Bundle/SimpleBTSBundle/Migrations/Schema/v1_1/OroAcademicSimpleBTSBundleMigration.php
+++ src/OroAcademic/Bundle/SimpleBTSBundle/Migrations/Schema/v1_1/OroAcademicSimpleBTSBundleMigration.php
@@ -3,17 +3,20 @@
 namespace OroCRM\Bundle\AccountBundle\Migrations\Schema\v1_1;
 
 use Doctrine\DBAL\Schema\Schema;
-
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtension;
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtensionAwareInterface;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
-
 use Oro\Bundle\NoteBundle\Migration\Extension\NoteExtension;
 use Oro\Bundle\NoteBundle\Migration\Extension\NoteExtensionAwareInterface;
 
-class OroSimpleBTSBundleMigration implements Migration, NoteExtensionAwareInterface
+class OroSimpleBTSBundleMigration implements Migration, NoteExtensionAwareInterface, ActivityExtensionAwareInterface
 {
     /** @var NoteExtension */
     protected $noteExtension;
+
+    /** @var ActivityExtension */
+    protected $activityExtension;
 
     /**
      * {@inheritdoc}
@@ -30,5 +33,25 @@ class OroSimpleBTSBundleMigration implements Migration, NoteExtensionAwareInterf
     public function up(Schema $schema, QueryBag $queries)
     {
         $this->noteExtension->addNoteAssociation($schema, 'oro_issue');
+        self::addActivityAssociations($schema, $this->activityExtension);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setActivityExtension(ActivityExtension $activityExtension)
+    {
+        $this->activityExtension = $activityExtension;
+    }
+
+    /**
+     * Enables Email activity for User entity
+     *
+     * @param Schema            $schema
+     * @param ActivityExtension $activityExtension
+     */
+    public static function addActivityAssociations(Schema $schema, ActivityExtension $activityExtension)
+    {
+        $activityExtension->addActivityAssociation($schema, 'oro_email', 'oro_issue', true);
     }
 }
